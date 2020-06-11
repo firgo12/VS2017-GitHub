271f985ff8b9a3c802a7adcd5add440e9bd8accb
firgo12 <60897048+firgo12@users.noreply.github.com>
2020-06-11 14:49:47 -0300

    Primeiro Commit

---
+++ TesteConsoleApp/TesteConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TesteConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de uso do GitHub com o Visual Studio");
+
+            Console.ReadKey();
+        }
+    }
+} 
